ddfbbd8e01689d135c5d9a3cb4e74491e759ee27
rajawaqar1 <waqarmalik953@gmail.com>
2023-04-04 15:57:46 +0500

    changes capslock function added

--- login.js
+++ login.js
@@ -1,1 +1,68 @@
 
+let password = document.getElementById("password");
+let pass = document.getElementById("pass");
+function toggel(elem) {
+    type = password.getAttribute("type");
+
+    if (isNaN(password.value)) {
+        alert("enter a number");
+
+    }
+    if (type === "password") {
+        password.setAttribute("type", "text");
+        elem.innerText = "hide password";
+
+    }
+    else {
+        password.setAttribute("type", "password");
+        elem.innerText = "show password";
+    }
+
+}
+function yes(ele) {
+    type = pass.getAttribute("type");
+    if (isNaN(pass.value)) {
+        alert("enter a number");
+    }
+    if (type === "password") {
+        pass.setAttribute("type", "text");
+        ele.innerText = "hide password";
+
+    }
+    else {
+        pass.setAttribute("type", "password");
+        ele.innerText = "show password";
+    }
+
+
+}
+function validate() {
+
+    if (pass.value != password.value) {
+        document.getElementById("res").innerHTML = "confirm password";
+    }
+    else {
+        document.getElementById("res").innerHTML = "";
+    }
+}
+function myFunction() {
+    yes(this);
+    toggel(this);
+}
+
+let input_txt = document.getElementById("input_txt");
+let done = document.getElementById("done");
+  window.addEventListener("keyup", event => {
+    let casps = event.getModifierState("CapsLock");
+    if (casps) {
+        document.getElementById("done").innerText = "Capslock is on!"
+
+    } else {
+        document.getElementById("done").innerHTML = "";
+    }
+})
+
+
+
+
+
